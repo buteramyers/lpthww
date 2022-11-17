9d45a574118f47452a0284c6668a6006491346a0
Myers <buteramyers96@gmail.com>
2022-11-17 12:22:03 +0200

    commit all

---
+++ ex29.py
@@ -0,0 +1,33 @@
+people = 40
+cats = 20 
+dogs = 25 
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs: 
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
+
+if people != dogs:
+    print("We need more animals")
+
+if people or cats:
+    print("Wow !!")

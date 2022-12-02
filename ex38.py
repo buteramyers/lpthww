36c8e6220ba7fcf28ef215aa8d96b5f483281c75
Myers <buteramyers96@gmail.com>
2022-12-02 09:08:51 +0200

    another commit

--- ex38.py
+++ ex38.py
@@ -3,7 +3,7 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 print("Wait there are not 10 things in that list. Let's fix that.")
 
 stuff = ten_things.split(' ')
-more_stuff = ["Davy", "Night", "Song", "Frisbee", 
+more_stuff = ["Day", "Night", "Song", "Frisbee", 
 "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:

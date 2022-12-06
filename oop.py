0ebedfbf6052d4c215f9f88a8c0bb31fbd432f39
Myers <buteramyers96@gmail.com>
2022-12-06 13:42:27 +0200

    commit this

--- oop.py
+++ oop.py
@@ -20,15 +20,53 @@ class Employee:
 class Developer(Employee):
     raise_amt = 1.10
 
-dev_1 = Developer('Myers', 'Butera', 50000)
-dev_2 = Developer('Test', 'Employee', 60000)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_employee(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+
+    def remove_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+dev_1 = Developer('Myers', 'Butera', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(issubclass(Manager, Developer))
+
+# print(mgr_1.email)
+
+# mgr_1.add_employee(dev_2)
+# mgr_1.remove_emp(dev_1)
+
+# mgr_1.print_emps() 
 
 # print(dev_1.email)
-# print(dev_2.email)
+# print(dev_1.prog_lang)
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
 
     # @classmethod
     # def set_raise_amt(cls, amount):

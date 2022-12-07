4397236bf2060c41f21468770006ec525e70d3fe
Myers <buteramyers96@gmail.com>
2022-12-07 12:30:26 +0200

    commit this

--- oop.py
+++ oop.py
@@ -7,27 +7,44 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay 
-        self.email = first + '.' + last + '@company.com'
 
         Employee.num_of_emps += 1
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname, self.email)
 
-    def __add__(self, other):
-        return self.pay + other.pay 
+    # def apply_raise(self):
+    #     self.pay = int(self.pay * self.raise_amount)
 
-    def __len__(self):
-        return len(self.fullname())
+    # def __repr__(self):
+    #     return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    # def __str__(self):
+    #     return '{} - {}'.format(self.fullname, self.email)
+
+    # def __add__(self, other):
+    #     return self.pay + other.pay 
+
+    # def __len__(self):
+    #     return len(self.fullname())
 
 # class Developer(Employee):
 #     raise_amt = 1.10
@@ -98,9 +115,17 @@ class Employee:
 # print(Employee.num_of_emps)
 
 emp_1 = Employee('Butera', 'Myers', 40000)
-emp_2 = Employee('Test', 'User', 30000)
+# emp_2 = Employee('Test', 'User', 30000)
 
-print(len('emp_1'))
+emp_1.fullname = 'Butera Myers'
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+
+del emp_1.fullname
+
+# print(len('emp_1'))
 
 
 # print(emp_1 + emp_2)

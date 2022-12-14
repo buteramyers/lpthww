fcd53addda93dbd3d7d1f71829579a445b691d01
Myers <buteramyers96@gmail.com>
2022-12-14 13:12:54 +0200

    commit all

--- ex44.py
+++ ex44.py
@@ -1,32 +1,32 @@
-class Parent(object):
+class Other(object):
 
     def override(self):
-        print("PARENT override()")
+        print("OTHER override()")
 
     def implicit(self):
-        print("PARENT implicit()")
+        print("OTHER implicit()")
 
     def altered(self):
-        print("PARENT altered")
+        print("OTHER altered")
 
-class Child(Parent):
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
 
     def override(self):
         print("CHILD override()")
     
     def altered(self):
         print("CHILD, BEFORE PARENT altered()")
-        super(Child, self).altered()
+        self.other.altered()
         print("CHILD, AFTER PARENT altered()")
 
-dad = Parent()
 son = Child()
 
-dad.implicit()
 son.implicit()
-
-dad.override()
 son.override()
-
-dad.altered()
 son.altered()
